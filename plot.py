02635e8ebf03f4a50037d40ebcbf8700003e9397
flossCoder <noreply@example.com>
2017-11-26 12:09:29 +0100

    Added further plots for er-graphs

--- plot.py
+++ plot.py
@@ -18,14 +18,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from math import ceil
+from math import ceil, factorial
 from myplot import *
 
 ## Format the axis in a standard way.
 def formatAxis():
-	plt.semilogy()
-	plt.xlabel("$d$")
-	plt.ylabel("$P(d)$")
+    plt.semilogy()
+    plt.xlabel("$d$")
+    plt.ylabel("$P(d)$")
 
 # Calculate the out name, which is required to save the results of the
 # simulation in a meaningfull way.
@@ -37,14 +37,14 @@ def formatAxis():
 #
 # @return The name snippet required to save the results.
 def outName(name, numberOfVertices, step, params):
-	out = name + "_" + str(numberOfVertices) + "_" + str(step)
-	for param in params:
-		if (ceil(param) == param):
-			out = out + "_" + str(int(ceil(param)))
-		else:
-			out = out + "_" + str(param)
-	
-	return(out)
+    out = name + "_" + str(numberOfVertices) + "_" + str(step)
+    for param in params:
+        if (ceil(param) == param):
+            out = out + "_" + str(int(ceil(param)))
+        else:
+            out = out + "_" + str(param)
+    
+    return(out)
 
 ## Plot a single histogram.
 #
@@ -59,19 +59,19 @@ def outName(name, numberOfVertices, step, params):
 #
 # @return The data of the histogram.
 def plotHistogram(directory, name, numberOfVertices, numberOfGraphs, params, fmt, **kwargs):
-	aux = outName(name, numberOfVertices, numberOfGraphs, params)
-	
-	# open histogram
-	d, pd, err = np.loadtxt(directory + "/hist_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	# remove the probabilities that are zero
-	d = d[pd != 0]
-	err = err[pd != 0]
-	pd = pd[pd != 0]
-	
-	plt.plot(d, pd, fmt, **kwargs)
-	
-	return([d, pd, err])
+    aux = outName(name, numberOfVertices, numberOfGraphs, params)
+    
+    # open histogram
+    d, pd, err = np.loadtxt(directory + "/hist_" + aux + ".csv", unpack = True, delimiter = " ")
+    
+    # remove the probabilities that are zero
+    d = d[pd != 0]
+    err = err[pd != 0]
+    pd = pd[pd != 0]
+    
+    plt.plot(d, pd, fmt, **kwargs)
+    
+    return([d, pd, err])
 
 ## Plot a single histogram with errorbars.
 #
@@ -85,19 +85,19 @@ def plotHistogram(directory, name, numberOfVertices, numberOfGraphs, params, fmt
 #
 # @return The data of the histogram.
 def plotHistogramError(directory, name, numberOfVertices, numberOfGraphs, params, **kwargs):
-	aux = outName(name, numberOfVertices, numberOfGraphs, params)
-	
-	# open histogram
-	d, pd, err = np.loadtxt(directory + "/hist_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	# remove the probabilities that are zero
-	d = d[pd != 0]
-	err = err[pd != 0]
-	pd = pd[pd != 0]
-	
-	errorbar(d, pd, err, **kwargs)
-	
-	return([d, pd, err])
+    aux = outName(name, numberOfVertices, numberOfGraphs, params)
+    
+    # open histogram
+    d, pd, err = np.loadtxt(directory + "/hist_" + aux + ".csv", unpack = True, delimiter = " ")
+    
+    # remove the probabilities that are zero
+    d = d[pd != 0]
+    err = err[pd != 0]
+    pd = pd[pd != 0]
+    
+    errorbar(d, pd, err, **kwargs)
+    
+    return([d, pd, err])
 
 ## Open the results of the two fits.
 #
@@ -107,15 +107,30 @@ def plotHistogramError(directory, name, numberOfVertices, numberOfGraphs, params
 # @param numberOfGraphs How many graphs have to be generated?
 # @param params Parameter array to generate the graph.
 def openFitResults(directory, name, numberOfVertices, numberOfGraphs, params):
-	aux = outName(name, numberOfVertices, numberOfGraphs, params)
-	
-	# open fit results pow
-	chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW = np.loadtxt(directory + "/pow_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	# open fit results normal
-	chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = np.loadtxt(directory + "/normal_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	return([chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL])
+    aux = outName(name, numberOfVertices, numberOfGraphs, params)
+    
+    # open fit results pow
+    rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW = [0,0,0,0,0,0,0,0,0]
+    try:
+        rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW = np.loadtxt(directory + "/pow_" + aux + ".csv", unpack = True, delimiter = " ")
+    except:
+        print("error for " + directory + "/pow_" + aux + ".csv")
+    
+    # open fit results normal
+    rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = [0,0,0,0,0,0,0]
+    try:
+        rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = np.loadtxt(directory + "/normal_" + aux + ".csv", unpack = True, delimiter = " ")
+    except:
+        print("error for " + directory + "/pow_" + aux + ".csv")
+    
+    # open fit results poisson
+    rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON = [0,0,0,0,0]
+    try:
+        rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON = np.loadtxt(directory + "/poisson_" + aux + ".csv", unpack = True, delimiter = " ")
+    except:
+        print("error for " + directory + "/poisson_" + aux + ".csv")
+    
+    return([rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL, rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON])
 
 ## Plot standard plot (with fits) for the given configuration.
 #
@@ -125,45 +140,45 @@ def openFitResults(directory, name, numberOfVertices, numberOfGraphs, params):
 # @param numberOfGraphs How many graphs have to be generated?
 # @param params Parameter array to generate the graph.
 def plot(directory, name, numberOfVertices, numberOfGraphs, params):
-	aux = outName(name, numberOfVertices, numberOfGraphs, params)
-	
-	# open fit results pow
-	chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW = np.loadtxt(directory + "/pow_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	# open fit results normal
-	chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = np.loadtxt(directory + "/normal_" + aux + ".csv", unpack = True, delimiter = " ")
-	
-	# plot the resulting histogram
-	plt.close()
-	fig,ax = plt.subplots(1)
-	
-	d, pd, err = plotHistogramError(directory, name, numberOfVertices, numberOfGraphs, params, fmt = "ok", capsize = 5, label = r"result")
-	
-	sep = len(d) / 1000.0
-	dfit = np.arange(1.0, (len(d) + sep), sep)
-	fitPow = k * (c / dfit) ** (l * dfit)
-	plt.plot(dfit, fitPow, "r", linestyle = "-.", label = r"$P(d) = k \left(\displaystyle\frac{C}{d}\right)^{\lambda d}$")
-	
-	if (sigma != 0):
-		fitNorm = np.exp(-(dfit - mu) ** 2 / (2.0 * sigma ** 2)) / np.sqrt(2.0 * np.pi * sigma ** 2)
-		fitNorm = fitNorm[fitNorm != np.Inf]
-		dfitN = dfit[fitNorm != np.Inf]
-		plt.plot(dfitN, fitNorm, "b", linestyle = "-", label = r"$P(d) = \displaystyle\frac{1}{\sqrt{2 \pi \sigma^2}} \exp\left( -\displaystyle\frac{(d - \mu)^2}{2 \sigma^2} \right)$")
-		if (any(fitNorm < 10**(-20))):
-			plt.ylim([10**(-20), max(fitNorm) + 1])
-	
-	# make legend nicer
-	handles,labels = ax.get_legend_handles_labels()
-	if (len(handles) == 3):
-		handles = [handles[2], handles[0], handles[1]]
-		labels = [labels[2], labels[0], labels[1]]
-	else:
-		handles = [handles[1], handles[0]]
-		labels = [labels[1], labels[0]]
-	plt.legend(handles, labels)
-	formatAxis()
-	
-	plt.savefig("%s/%s.pdf"%(directory, aux))
+    aux = outName(name, numberOfVertices, numberOfGraphs, params)
+    
+    # open fit results
+    [rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL, rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON] = openFitResults(directory, name, numberOfVertices, numberOfGraphs, params)
+    
+    # plot the resulting histogram
+    plt.close()
+    fig,ax = plt.subplots(1)
+    
+    d, pd, err = plotHistogramError(directory, name, numberOfVertices, numberOfGraphs, params, fmt = "ok", capsize = 5, label = r"result")
+    
+    sep = len(d) / 1000.0
+    dfit = np.arange(1.0, (len(d) + sep), sep)
+    dfitInt = np.arange(1, (len(d) + 1), 1)
+    fitPow = k * (c / dfit) ** (l * dfit)
+    fitPoisson = np.array([lam**x / factorial(x) * np.exp(-lam) for x in dfitInt])
+    plt.plot(dfit, fitPow, "r", linestyle = "-.", label = r"$P(d) = k \left(\displaystyle\frac{C}{d}\right)^{\lambda d}$")
+    plt.plot(dfitInt, fitPoisson, "g", linestyle = ":", label = r"$P(d) = \displaystyle\frac{\lambda^d}{d!} \cdot \exp\left(-\lambda\right)$")
+    
+    if (sigma != 0):
+        fitNorm = np.exp(-(dfit - mu) ** 2 / (2.0 * sigma ** 2)) / np.sqrt(2.0 * np.pi * sigma ** 2)
+        fitNorm = fitNorm[fitNorm != np.Inf]
+        dfitN = dfit[fitNorm != np.Inf]
+        plt.plot(dfitN, fitNorm, "b", linestyle = "-", label = r"$P(d) = \displaystyle\frac{1}{\sqrt{2 \pi \sigma^2}} \exp\left( -\displaystyle\frac{(d - \mu)^2}{2 \sigma^2} \right)$")
+        if (any(fitNorm < 10**(-20))):
+            plt.ylim([10**(-20), max(fitNorm) + 1])
+    
+    # make legend nicer
+    handles,labels = ax.get_legend_handles_labels()
+    #if (len(handles) == 3):
+    #    handles = [handles[2], handles[0], handles[1]]
+    #    labels = [labels[2], labels[0], labels[1]]
+    #else:
+    #    handles = [handles[1], handles[0]]
+    #    labels = [labels[1], labels[0]]
+    plt.legend(handles, labels)
+    formatAxis()
+    
+    plt.savefig("%s/%s.pdf"%(directory, aux))
 
 plt.rcParams['figure.figsize'] = (7.0, 7.0) # change the size of the plot
 plt.rcParams['font.size'] = (14)
@@ -176,20 +191,12 @@ name = "pa"
 # Prepare the latex table containing the configuration and chi^2 / df for both fits.
 file = open(directory + "/%s_tab.tex"%(name), "w")
 for numberOfVertices in [50, 100, 200, 400]:
-	for numberOfGraphs in [1000, 3500, 6000, 8500, 10000]:
-		for numEdges in [1, 2]:
-			# open fit results
-			chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = openFitResults(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
-			# save the configuration and chi^2 / df as latex table line
-			if ((chisquaredfPOW == np.float64(0.0)) & (chisquaredfNORMAL == np.float64(0.0))):
-				file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & --- & --- \\\\\n')
-			elif (chisquaredfPOW == np.float64(0.0)):
-				file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & --- & \\num{' + '{:0.3e}'.format(chisquaredfNORMAL) + '} \\\\\n')
-			elif (chisquaredfNORMAL == np.float64(0.0)):
-				file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & \\num{' + '{:0.3e}'.format(chisquaredfPOW) + '} & --- \\\\\n')
-			else:
-				file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & \\num{' + '{:0.3e}'.format(chisquaredfPOW) + '} & \\num{' + '{:0.3e}'.format(chisquaredfNORMAL) + '} \\\\\n')
-			plot(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
+    for numberOfGraphs in [1000, 3500, 6000, 8500, 10000]:
+        for numEdges in [1, 2]:
+            # open fit results
+            rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL, rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON = openFitResults(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
+            file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & \\num{' + '{:0.3e}'.format(rPOW) + '} & \\num{' + '{:0.3e}'.format(rNORMAL) + '} & \\num{' + '{:0.3e}'.format(rPOISSON) + '} \\\\\n')
+            plot(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
 file.close()
 
 # plot numberOfGraphs = 10000
@@ -331,21 +338,13 @@ name = "ca"
 # Prepare the latex table containing the configuration and chi^2 / df for both fits.
 file = open(directory + "/%s_tab.tex"%(name), "w")
 for numberOfVertices in [50, 100, 200, 400]:
-	for numberOfGraphs in [1000, 3500, 6000, 8500, 10000]:
-		for numEdges in [1, 2]:
-			for probability in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-				# open fit results
-				chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL = openFitResults(directory, name, numberOfVertices, numberOfGraphs, [numEdges, probability])
-				# save the configuration and chi^2 / df as latex table line
-				if ((chisquaredfPOW == np.float64(0.0)) & (chisquaredfNORMAL == np.float64(0.0))):
-					file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & ' + str(probability) + ' & --- & --- \\\\\n')
-				elif (chisquaredfPOW == np.float64(0.0)):
-					file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & ' + str(probability) + ' & --- & \\num{' + '{:0.3e}'.format(chisquaredfNORMAL) + '} \\\\\n')
-				elif (chisquaredfNORMAL == np.float64(0.0)):
-					file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & ' + str(probability) + ' & \\num{' + '{:0.3e}'.format(chisquaredfPOW) + '} & --- \\\\\n')
-				else:
-					file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & ' + str(probability) + ' & \\num{' + '{:0.3e}'.format(chisquaredfPOW) + '} & \\num{' + '{:0.3e}'.format(chisquaredfNORMAL) + '} \\\\\n')
-				plot(directory, name, numberOfVertices, numberOfGraphs, [numEdges, probability])
+    for numberOfGraphs in [1000, 3500, 6000, 8500, 10000]:
+        for numEdges in [1, 2]:
+            for probability in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+                # open fit results
+                rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL, rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON = openFitResults(directory, name, numberOfVertices, numberOfGraphs, [numEdges, probability])
+                file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & ' + str(probability) + ' & \\num{' + '{:0.3e}'.format(rPOW) + '} & \\num{' + '{:0.3e}'.format(rNORMAL) + '} & \\num{' + '{:0.3e}'.format(rPOISSON) + '} \\\\\n')
+                plot(directory, name, numberOfVertices, numberOfGraphs, [numEdges, probability])
 file.close()
 
 numberOfGraphs = 10000
@@ -1563,6 +1562,52 @@ formatAxis()
 plt.semilogy()
 plt.legend()
 plt.savefig("%s/%s_const-numberOfVertices_%s-m_%s-p_%s.pdf"%(directory, name, str(numberOfVertices), str(m), str(probability)))
+
+
+
+
+
+name = "er"
+
+
+
+
+
+# Prepare the latex table containing the configuration and chi^2 / df for both fits.
+file = open(directory + "/%s_tab.tex"%(name), "w")
+for numberOfVertices in [50, 100, 200, 400]:
+    for numberOfGraphs in [1000, 3500, 6000, 8500, 10000]:
+        for numEdges in [1, 2]:
+            # open fit results
+            rPOW, chisquaredfPOW, k, kerr, c, cerr, l, lerr, dfPOW, rNORMAL, chisquaredfNORMAL, mu, muerr, sigma, sigmaerr, dfNORMAL, rPOISSON, chisquaredfPOISSON, lam, lamerr, dfPOISSON = openFitResults(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
+            file.write(str(numberOfVertices) + ' & ' + str(numberOfGraphs) + ' & ' + str(numEdges) + ' & \\num{' + '{:0.3e}'.format(rPOW) + '} & \\num{' + '{:0.3e}'.format(rNORMAL) + '} & \\num{' + '{:0.3e}'.format(rPOISSON) + '} \\\\\n')
+            plot(directory, name, numberOfVertices, numberOfGraphs, [numEdges])
+file.close()
+
+# plot numberOfGraphs = 10000
+plt.close()
+fig,ax = plt.subplots(1)
+# plot m = 1
+m = 1
+plotHistogram(directory, name, 50, 10000, [m], "+b", label = r"$\left|V\right| = 50,~m = 1$")
+plotHistogram(directory, name, 100, 10000, [m], "+k", label = r"$\left|V\right| = 100,~m = 1$")
+plotHistogram(directory, name, 200, 10000, [m], "+r", label = r"$\left|V\right| = 200,~m = 1$")
+plotHistogram(directory, name, 400, 10000, [m], "+m", label = r"$\left|V\right| = 400,~m = 1$")
+
+# plot m = 2
+m = 2
+plotHistogram(directory, name, 50, 10000, [m], "*b", label = r"$\left|V\right| = 50,~m = 2$")
+plotHistogram(directory, name, 100, 10000, [m], "*k", label = r"$\left|V\right| = 100,~m = 2$")
+plotHistogram(directory, name, 200, 10000, [m], "*r", label = r"$\left|V\right| = 200,~m = 2$")
+plotHistogram(directory, name, 400, 10000, [m], "*m", label = r"$\left|V\right| = 400,~m = 2$")
+formatAxis()
+plt.semilogy()
+plt.legend()
+plt.savefig("%s/%s_const-numberOfGraphs_%s.pdf"%(directory, name, str(numberOfGraphs)))
+
+
+
+
 
 
 numberOfVertices = 50
